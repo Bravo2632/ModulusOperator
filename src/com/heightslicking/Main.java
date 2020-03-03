03be9dc0797798e79308236da809ae0973d80721
Brandon Gibson <Brandon.gibson.2022@student.lhschools.org>
2020-03-03 14:16:47 -0500

    keepContainedMethodCreated

--- src/com/heightslicking/Main.java
+++ src/com/heightslicking/Main.java
@@ -7,23 +7,34 @@ public class Main {
     public static void main(String[] args) {
 
         testOddness();
-
+        keepContained();
     }
-        public static void testOddness(){	// write your code here
-    //todo even or odd
+        public static void testOddness() {    // write your code here
+            //todo even or odd
+            //todo keep number contained
+            //todo get the last digit
+
+            Scanner keyboard;
+            int positiveNumber;
+
+
+            keyboard = new Scanner(System.in);
+            System.out.println("Give me a positive number.");
+            positiveNumber = keyboard.nextInt();
+            System.out.println("Number is even: " + (positiveNumber % 2 == 0));
+            System.out.println("Number is odd: " + (positiveNumber % 2 != 0));
+
+        }
+        public static void keepContained(){
         //todo keep number contained
-        //todo get the last digit
 
-        Scanner keyboard;
-        int positiveNumber;
+            Scanner keyboard;
+            int positiveNumber2;
 
-
-
-        keyboard = new Scanner(System.in);
-        System.out.println("Give me a positive number.");
-        positiveNumber = keyboard.nextInt();
-        System.out.println("Number is even: "+(positiveNumber % 2 ==0));
-        System.out.println("Number is odd: "+ (positiveNumber % 2 !=0));
+            keyboard = new Scanner(System.in);
+            System.out.println("I need a positive number.");
+            positiveNumber2 = keyboard.nextInt();
+            System.out.println(""+positiveNumber2);
 
 
 
